8867824a4c37c7cde10179682568c20c75504156
Sweg Swog <ibyanli@gmail.com>
2017-05-12 03:05:11 -0700

    First Map First map before island, placeholder sprite rn. Guy walks and has collisions.

---
+++ Assets/Tiled2Unity/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public Transform target;
+	Camera mycam;
+	public float m_speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		mycam = GetComponent<Camera> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// orthographicSize correlates to "size" in camera
+		// f just makes it a float, so it's 100.0
+		// ensures game scale is the same across devices
+		mycam.orthographicSize = (Screen.height / 100f) / 5f;
+
+		if(target){
+			transform.position = Vector3.Lerp(transform.position, target.position + new Vector3(0,-0.5f,-10), m_speed);
+		}
+	}
+}
---
+++ Assets/Tiled2Unity/Scripts/Warp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+
+	public Transform warpTarget;
+	public BoxCollider2D warpBox;
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		Debug.Log ("Object Collided");
+
+		// dumb way to check if x or y matters when warping being the warpBox
+		if (warpBox.size.x > warpBox.size.y) {
+			//if collide, moves to warp target
+			other.gameObject.transform.position = new Vector3 (other.gameObject.transform.position.x, warpTarget.position.y, warpTarget.position.z);
+			//move cam too
+			Camera.main.transform.position = warpTarget.position;
+		} else {
+			other.gameObject.transform.position = new Vector3 (warpTarget.position.x, other.gameObject.transform.position.y, warpTarget.position.z);
+			Camera.main.transform.position = warpTarget.position;
+		}
+	}
+
+}
